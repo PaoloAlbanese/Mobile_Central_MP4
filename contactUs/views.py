22231d56c6836dfba565fc782059b55681bf63e3
PaoloAlbanese <paulitus_@hotmail.com>
2020-11-17 11:40:00 +0000

    testing email on heroku

--- contactUs/views.py
+++ contactUs/views.py
@@ -40,8 +40,8 @@ def contact(request):
             email_from = settings.EMAIL_HOST_USER
             recipient_list = ['paulitus_@hotmail.com',]
 
-            send_mail( subject, message, email_from, recipient_list ),
-            return render(request, 'contactUs/contact_success.html', {'message': message, 'name': captName, 'emailTo': from_email, 'subject': subject, 'emailJSid': emailJSid})
+            send_mail( subject, message, email_from, recipient_list),
+            # return render(request, 'contactUs/contact_success.html', {'message': message, 'name': captName, 'emailTo': from_email, 'subject': subject, 'emailJSid': emailJSid})
 
             return render(request, 'contactUs/contact_success.html', {'message': message, 'name': captName, 'emailTo': from_email, 'subject': subject, 'emailJSid': emailJSid})
 
@@ -51,31 +51,31 @@ def contact(request):
         else:
             form = ContactForm()
 
-    api_key = 'f3afd5c712e2a861966c4fa4e58be6b0'
-    api_secret = 'd88604054ab22dbd591f4ae99d101a7e'
-    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
-    data = {
-        'Messages': [
-            {
-                "From": {
-                    "Email": "paulitus_@hotmail.com",
-                    "Name": "paolo"
-                },
-                "To": [
-                    {
-                        "Email": "paulitus_@hotmail.com",
-                        "Name": "paolo"
-                    }
-                ],
-                "Subject": "Greetings from Mailjet.",
-                "TextPart": "My first Mailjet email",
-                "HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
-                "CustomID": "AppGettingStartedTest"
-            }
-        ]
-    }
-    result = mailjet.send.create(data=data)
-    print ('result staus code',result.status_code)
-    print ('json result',result.json())
+    # api_key = 'f3afd5c712e2a861966c4fa4e58be6b0'
+    # api_secret = 'd88604054ab22dbd591f4ae99d101a7e'
+    # mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+    # data = {
+    #     'Messages': [
+    #         {
+    #             "From": {
+    #                 "Email": "paulitus_@hotmail.com",
+    #                 "Name": "paolo"
+    #             },
+    #             "To": [
+    #                 {
+    #                     "Email": "paulitus_@hotmail.com",
+    #                     "Name": "paolo"
+    #                 }
+    #             ],
+    #             "Subject": "Greetings from Mailjet.",
+    #             "TextPart": "My first Mailjet email",
+    #             "HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+    #             "CustomID": "AppGettingStartedTest"
+    #         }
+    #     ]
+    # }
+    # result = mailjet.send.create(data=data)
+    # print ('result staus code',result.status_code)
+    # print ('json result',result.json())
 
     return render(request, 'contactUs/contact.html', {'form': form})
